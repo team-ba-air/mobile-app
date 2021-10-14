e363bd8f6621867049d010a6fd1b9cae0e8eea78
Yahya1547 <1547yahya@gmail.com>
2021-10-14 21:11:47 +0700

    implement home and service list

--- src/navigations/index.tsx
+++ src/navigations/index.tsx
@@ -2,11 +2,13 @@ import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import AppNavigator from "./app-navigator";
 import BottomNavigator from "./bottom-navigator";
 import OnboardingNavigator from "./onboarding-navigator";
+import ReservationNavigator from "./reservation-navigator";
 
 const RootNavigator = createSwitchNavigator(
   {
     Onboarding: OnboardingNavigator,
     App: BottomNavigator,
+    Reservation: ReservationNavigator,
   },
   {
     initialRouteName: 'Onboarding',
---
+++ src/scenes/reservation/ServiceReservation.tsx
@@ -0,0 +1,62 @@
+import AppContainer from 'components/AppContainer';
+import React from 'react'
+import { ListRenderItemInfo, Text, View } from 'react-native';
+import { Card } from 'react-native-elements';
+import { FlatList } from 'react-native-gesture-handler';
+import { ServiceItem } from './constants';
+
+interface ServiceReservationProps {
+  
+}
+
+const serviceList: ServiceItem[] = [
+  {
+    img: 'servis_logbook.png',
+    value: 'logbook',
+    label: 'Servis Logbook'
+  },
+  {
+    img: 'servis_dasar.png',
+    value: 'basic',
+    label: 'Servis dasar'
+  },
+  {
+    img: 'servis_ac',
+    value: 'AC',
+    label: 'Servis AC'
+  },
+  {
+    img: '',
+    value: 'clutch',
+    label: 'Servis Clutch'
+  },
+  {
+    img: '',
+    value: 'oil',
+    label: 'Servis oli'
+  },
+]
+
+const ServiceReservation: React.FC<ServiceReservationProps> = () => {
+  return ( 
+    <AppContainer>
+      <View>
+        <Text>Buat mobil</Text>
+        <Text>Yaris B 2000 S</Text>
+      </View>
+      <FlatList
+        data={serviceList}
+        renderItem={(item: ListRenderItemInfo<ServiceItem>) => (
+          <Card>
+            <Card.Image source={require(`@assets/servis_dasar.png`)} />
+            <View>
+              <Text>{item.item.label}</Text>
+            </View>
+          </Card>
+        )}
+      />
+    </AppContainer>
+   );
+}
+ 
+export default ServiceReservation;
