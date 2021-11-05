ad935989d40d7bb13dc19d775fde59e977799341
Yahya1547 <1547yahya@gmail.com>
2021-11-06 00:04:43 +0700

    continue profile

--- src/navigations/app-navigator.tsx
+++ src/navigations/app-navigator.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import Icon from 'react-native-vector-icons/Ionicons';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeScreen from "scenes/home";
 import StartOnboarding from 'scenes/onboarding/StartOnboarding';
-import Home from 'assets/icon/home.svg'
+import ProfileScreen from 'scenes/profile/ProfileScreen';
 
 const BottomNavigator = createBottomTabNavigator()
 
 const AppNavigator = () => {
   return <BottomNavigator.Navigator>
-      <BottomNavigator.Screen options={{ headerShown: false }} name='HomeTab' component={HomeScreen} />
-      <BottomNavigator.Screen name='History' component={StartOnboarding} />
+      <BottomNavigator.Screen options={{ headerShown: false, tabBarIcon: () => <Icon name='home' size={30}/>, tabBarLabel: 'Home' }} name='HomeTab' component={HomeScreen} />
+      <BottomNavigator.Screen name='Profile' component={ProfileScreen} />
     </BottomNavigator.Navigator>
 }
 
---
+++ src/scenes/profile/ProfileScreen.tsx
@@ -0,0 +1,78 @@
+import AppContainer from 'components/AppContainer';
+import CustomButton from 'components/CustomButton';
+import React from 'react'
+import { StyleSheet, View } from 'react-native';
+import { Image, Text } from 'react-native-elements';
+import { Color } from 'styles/colors';
+import { Sizing } from 'styles/sizes';
+import ProfileAction from './components/ProfileAction';
+
+interface ProfileScreenProps {
+  
+}
+ 
+const ProfileScreen: React.FC<ProfileScreenProps> = () => {
+  return ( 
+    <AppContainer style={styles.container}>
+      <View>
+        <View style={styles.infoContainer}>
+          <View>
+            <Text style={styles.headerText}>Oto Suroto</Text>
+            <Text style={styles.normalText}>otosuroto@gmail.com</Text>
+            <Text style={styles.normalText}>0812 3456 7891</Text>
+          </View>
+          <View>
+            <CustomButton buttonStyle={styles.button} textStyle={{ fontSize: Sizing.text.body[12], fontWeight: 'bold' }} title='Detail'/>
+          </View>
+        </View>
+
+        <ProfileAction />
+      </View>
+      <View style={styles.containerBottom}>
+        <Text>Ver 1.0.0</Text>
+        <Image source={require('assets/logo_header.png')} />
+        <CustomButton title='Logout' />
+      </View>
+    </AppContainer>
+   );
+}
+ 
+export default ProfileScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    backgroundColor: Color.gray[1],
+    padding: 0,
+  },
+  normalText: {
+    fontSize: Sizing.text.body[14],
+  },
+  headerText: {
+    fontSize: Sizing.text.body[16],
+    fontWeight: 'bold',
+  },
+  button: { 
+    paddingTop: 6,
+    paddingBottom: 6,
+    paddingLeft: 24, 
+    paddingRight: 24, 
+    borderRadius: 8 
+  },
+  infoContainer: { 
+    display: 'flex', 
+    flexDirection: 'row', 
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    backgroundColor: Color.gray[0],
+    marginBottom: 24,
+  },
+  containerBottom: {
+    paddingHorizontal: 20,
+    paddingBottom: 16,
+  }
+})
