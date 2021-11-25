d29132ffc0d6fadd8ec1503bf32fb462eb2b1fb2
Yahya1547 <1547yahya@gmail.com>
2021-11-26 00:38:43 +0700

    change icon nav for mobil

--- src/navigations/app-navigator.tsx
+++ src/navigations/app-navigator.tsx
@@ -16,7 +16,7 @@ const AppNavigator = () => {
         options={{ 
           headerShown: false, 
           tabBarIcon: () => <Image source={require('@assets/icon/ic_home.png')} style={{ width: 20, height: 20 }} resizeMode={'contain'} />, 
-          tabBarLabel: 'Home' 
+          tabBarLabel: 'Home',
         }} 
         name='HomeTab' 
         component={HomeScreen} 
@@ -24,13 +24,16 @@ const AppNavigator = () => {
       <BottomNavigator.Screen 
         options={{
           headerShown: false,
+          tabBarIcon: () => <Image source={require('@assets/icon/ic_car_nav.png')} style={{ width: 20, height: 20 }} resizeMode={'contain'} />,
+          tabBarLabel: 'Mobil',
         }}
         name='Mobil' 
         component={VehicleScreen} 
       />
       <BottomNavigator.Screen 
         options={{ 
-          tabBarIcon: () => <Image source={require('@assets/icon/ic_profile.png')} style={{ width: 20, height: 20 }} resizeMode={'contain'} /> 
+          tabBarIcon: () => <Image source={require('@assets/icon/ic_profile.png')} style={{ width: 20, height: 20 }} resizeMode={'contain'} />,
+          tabBarLabel: 'Profil',
         }} 
         name='Profile'
         component={ProfileScreen}
