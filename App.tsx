dbffee25b63806af749484d0fd98cd76345ad952
Yahya1547 <1547yahya@gmail.com>
2021-12-09 12:00:26 +0700

    create splash screen

--- App.tsx
+++ App.tsx
@@ -6,17 +6,28 @@
  * @flow strict-local
  */
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
   StyleSheet,
 } from 'react-native';
 import Navigator from 'navigations'
 import AuthorizationProvider from 'context/AuthorizationProvider';
 import { NavigationContainer } from '@react-navigation/native';
+import { GoogleSignin } from 'react-native-google-signin';
 
 type AppProps = {}
 
 const App: React.FC<AppProps> = () => {
+  useEffect(() => {
+    GoogleSignin.configure({
+      scopes: [],
+      webClientId: '75808358640-7phfh1dhh5eqtnvt84vebs41m87cp660.apps.googleusercontent.com',
+      offlineAccess: true,
+      hostedDomain: '',
+      loginHint: '',
+      forceConsentPrompt: true,
+    })
+  }, [])
   return (
     <AuthorizationProvider>
       <NavigationContainer>
