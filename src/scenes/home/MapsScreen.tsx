14e677bf9af1d901d9661cdab4fe49001addaa43
Yahya1547 <1547yahya@gmail.com>
2022-03-29 02:25:15 +0700

    update package geolocation and get permission

--- src/scenes/home/MapsScreen.tsx
+++ src/scenes/home/MapsScreen.tsx
@@ -1,27 +1,75 @@
 import AppContainer from 'components/AppContainer';
-import React from 'react'
-import { StyleSheet } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import React, { useEffect, useState } from 'react'
+import { PermissionsAndroid, StyleSheet } from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
 
 interface MapsScreenProps {
     
 }
+
+export type LocationPoint = {
+  latitude: number
+  longitude: number
+}
  
 const MapsScreen: React.FC<MapsScreenProps> = () => {
-    return ( 
-      <AppContainer>
-        <MapView
-          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-          style={styles.map}
-          region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}
-        ></MapView>
-      </AppContainer>
-    );
+  const [location, setLocation] = useState<LocationPoint | null>(null)
+  useEffect(() => {
+    PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: "Cool Photo App Camera Permission",
+        message:
+          "Cool Photo App needs access to your camera " +
+          "so you can take awesome pictures.",
+        buttonNeutral: "Ask Me Later",
+        buttonNegative: "Cancel",
+        buttonPositive: "OK"
+      }).then(permission => {
+        if (permission === PermissionsAndroid.RESULTS.GRANTED) {
+          getLocation()
+        } else {
+          console.log("Camera permission denied");
+        }
+      })
+  }, [])
+
+  const getLocation = () => {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        setLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        })
+        console.log(position);
+      },
+      (error) => {
+        // See error code charts in documentation.
+        console.log(error.code, error.message);
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+  );
+  }
+  return ( 
+    <AppContainer>
+      <MapView
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        region={{
+          latitude: location?.latitude ?? 37.78825,
+          longitude: location?.longitude ?? -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      >
+        {location && (
+          <Marker 
+            coordinate={location}
+          />
+        )}
+      </MapView>
+    </AppContainer>
+  );
 }
  
 export default MapsScreen;
