6d594d43b61b972d62cfb7ee10f6236df36224e6
Yahya1547 <1547yahya@gmail.com>
2022-04-16 07:31:24 +0700

    show bottom sheet review

--- src/scenes/history/components/HistoryItemComponent.tsx
+++ src/scenes/history/components/HistoryItemComponent.tsx
@@ -2,30 +2,41 @@ import CustomButton from 'components/CustomButton';
 import React from 'react'
 import { ProgressViewIOSComponent, View } from 'react-native';
 import { Text } from 'react-native-elements';
+import { Color } from 'styles/colors';
+import { fontPixel, heightPixel } from 'styles/sizes';
 import { getFormatDate } from 'utils/DateUtil';
 import { HistoryItem } from '../constants';
 
 interface HistoryItemComponentProps {
   item: HistoryItem
+  setIsOpenReview: (open: boolean) => void
+  setData: (data: HistoryItem) => void
 }
  
-const HistoryItemComponent: React.FC<HistoryItemComponentProps> = ({ item }) => {
+const HistoryItemComponent: React.FC<HistoryItemComponentProps> = ({ item, setIsOpenReview, setData }) => {
   return ( 
-    <View>
+    <View style={{ backgroundColor: Color.gray[0], borderRadius: 8, padding: 16, marginBottom: heightPixel(8) }}>
       <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-        <Text>{item.serviceType}</Text>
-        <Text>{item.price}</Text>
+        <Text style={{ fontSize: fontPixel(14) }}>{item.serviceType}</Text>
+        <Text style={{ fontSize: fontPixel(14) }}>{item.price}</Text>
       </View>
 
-      <Text>{item.carType} {item.plat}</Text>
+      <Text style={{ fontSize: fontPixel(12), color: Color.gray[6], marginBottom: heightPixel(16) }}>{item.carType} {item.plat}</Text>
 
-      <View>
+      <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
         <View>
-          <Text>{getFormatDate(item.date)}</Text>
-          <Text>{item.location}</Text>
+          <Text style={{ fontSize: fontPixel(10), color: Color.gray[6] }}>{getFormatDate(item.date)}</Text>
+          <Text style={{ fontSize: fontPixel(10), color: Color.gray[6] }}>{item.location}</Text>
         </View>
 
-        <CustomButton type='primary' title={'Beri Ulasan'}/>
+        <CustomButton 
+          textStyle={{ fontSize: fontPixel(12) }} 
+          type='primary' 
+          title={'Beri Ulasan'} 
+          onPress={() => {
+            setData(item)
+            setIsOpenReview(true)
+          }} />
       </View>
     </View>
   );
