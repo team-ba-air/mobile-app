675705130cb61fe293e23d0b8659c61d36486904
Yahya1547 <1547yahya@gmail.com>
2022-04-18 05:20:41 +0700

    create layout history servis detail

--- src/scenes/history/history-detail/HistoryDetailScreen.tsx
+++ src/scenes/history/history-detail/HistoryDetailScreen.tsx
@@ -1,7 +1,10 @@
 import AppContainer from 'components/AppContainer';
 import React from 'react'
+import { ScrollView } from 'react-native';
+import { Color } from 'styles/colors';
 import HistoryStatusComponent from './components/HistoryStatusComponent';
 import InfoServiceComponent from './components/InfoServiceComponent';
+import NotesComponent from './components/NotesComponent';
 import PaymentDetailComponent from './components/PaymentDetailComponent';
 
 interface HistoryDetailScreenProps {
@@ -10,10 +13,13 @@ interface HistoryDetailScreenProps {
  
 const HistoryDetailScreen: React.FC<HistoryDetailScreenProps> = () => {
   return ( 
-    <AppContainer>
-      <HistoryStatusComponent />
-      <InfoServiceComponent />
-      <PaymentDetailComponent />
+    <AppContainer style={{ backgroundColor: Color.gray[1], padding: 0 }}>
+      <ScrollView>
+        <HistoryStatusComponent />
+        <InfoServiceComponent />
+        <PaymentDetailComponent />
+        <NotesComponent />
+      </ScrollView>
     </AppContainer>
   );
 }
--- src/scenes/history/history-detail/components/NotesComponent.tsx
+++ src/scenes/history/history-detail/components/NotesComponent.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Text, View } from 'react-native';
 import { Color } from 'styles/colors';
-import { fontPixel } from 'styles/sizes';
+import { fontPixel, heightPixel, widthPixel } from 'styles/sizes';
 
 interface NotesComponentProps {
     
@@ -9,7 +9,7 @@ interface NotesComponentProps {
  
 const NotesComponent: React.FC<NotesComponentProps> = () => {
   return ( 
-    <View>
+    <View style={{ backgroundColor: 'white', paddingHorizontal: widthPixel(20), paddingVertical: heightPixel(16), marginBottom: heightPixel(8) }}>
       <Text style={{ fontSize: fontPixel(14), color: Color.gray.secondary }}>Catatan dari Bengkel</Text>
       <Text style={{ fontSize: fontPixel(14) }}>
         {'V-Belt dan Kampas Rem Bapak/Ibu sudah terlalu lama tidak diganti, kalau terlalu lama dibiarkan bisa merusak komponen lain. Untuk Filter sudah tidak optimal juga, tapi masih bisa tahan ~3 bulan kedepan.'}
