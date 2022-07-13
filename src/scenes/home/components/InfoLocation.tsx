386eb85290f7a8dbbec8ffd606e26db0c2b3810d
Yahya1547 <1547yahya@gmail.com>
2022-07-13 08:05:15 +0700

    fix text size

--- src/scenes/home/components/InfoLocation.tsx
+++ src/scenes/home/components/InfoLocation.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Icon, Text } from 'react-native-elements';
 import { Color } from 'styles/colors';
-import { Sizing } from 'styles/sizes';
+import { fontPixel, Sizing } from 'styles/sizes';
 import Geocoder from 'react-native-geocoder';
 
 interface InfoLocationProps {
@@ -33,13 +33,13 @@ const InfoLocation: React.FC<InfoLocationProps> = ({ navigation }) => {
   return ( 
     <TouchableOpacity style={styles.container} onPress={() => navigation.navigate(SCREENS.app.maps)}>
       <View>
-        <Text style={{ fontSize: Sizing.text.subheading[18], color: Color.gray[0], fontWeight: 'bold' }}>Hi, Oto</Text>
+        <Text style={{ fontSize: fontPixel(Sizing.text.subheading[18]), color: Color.gray[0], fontWeight: 'bold' }}>Hi, Oto</Text>
       </View>
       <View style={{ marginTop: 12, flexDirection: 'row', alignItems: 'center' }}>
         <Icon type='material' name='location-on' color={Color.red[4]} tvParallaxProperties={undefined} />
         <View>
-          <Text style={{ fontSize: Sizing.text.body[11], color: Color.blue[1]}}>Lokasi Anda</Text>
-          <Text style={{ fontSize: Sizing.text.body[12], color: Color.gray[0], fontWeight: 'bold' }}>{address ? address : '-'}</Text>
+          <Text style={{ fontSize: fontPixel(Sizing.text.body[11]), color: Color.blue[1]}}>Lokasi Anda</Text>
+          <Text style={{ fontSize: fontPixel(Sizing.text.body[12]), color: Color.gray[0], fontWeight: 'bold' }}>{address ? address : '-'}</Text>
         </View>
       </View>
     </TouchableOpacity>
